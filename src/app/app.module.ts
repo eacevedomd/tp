b003eeb27a0e8d0f3e2805282a18bef6298def03
Emiliano Acevedo <eacevedo@mobydigital.com>
2023-12-26 10:43:40 -0300

    created component Saludo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaludoComponent } from './components/saludo/saludo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaludoComponent
   ],
   imports: [
     BrowserModule,
